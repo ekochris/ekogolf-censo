f817aa6c7d10ba08527475ccfddbb2ad9181d0bc
Christian Carreno <totoyin_88@hotmail.com>
2025-05-20 18:28:04 -0600

    Corrige nombre del archivo de configuración de Cloudinary

---
+++ config_cloudinary.py
@@ -0,0 +1,8 @@
+import cloudinary
+
+cloudinary.config(
+    cloud_name = "dr7piopew",
+    api_key = "251591238998854",
+    api_secret = "RTo5IkliPlz8wxQZrY4_VaJLW-0",
+    secure = True
+)
--- import_cloudinary.py
+++
@@ -1,8 +0,0 @@
-import cloudinary
-
-cloudinary.config(
-  cloud_name='tu_cloud_name',
-  api_key='tu_api_key',
-  api_secret='tu_api_secret',
-  secure=True
-)
